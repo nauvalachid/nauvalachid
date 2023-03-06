11fac735d6c7334861f28a5d5516e75f0b194768
nauvalachid <nauvalachid@gmail.com>
2023-03-06 12:06:59 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Begin'
+    // Numeric Luas,p,l
+    double Luas,p,l;
+    // display 'Masukan Panjangnya = '
+    cout << "Masukan Panjangnya = ";
+    // Accept p
+    cin >> p;
+    // display 'Masukan Lebarnya = '
+    cout << "Masukan Lebarnya = ";
+    // Accept l
+    cin >> l;
+    // Compute luas = p*l
+    cout << "Compute luas = p*l ";
+    Luas = p*l;
+    // display "Luasnya = "+Luas
+    cout << "Luasnya = " << Luas;
+    // End
+
+
+    
+
+}
